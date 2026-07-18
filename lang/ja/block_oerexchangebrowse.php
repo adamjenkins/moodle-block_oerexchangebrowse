b873c0579ff7b3bec2eb305e21b66977364b3b8f
Adam Jenkins <adam@wisecat.net>
2026-07-18 10:56:18 +0000

    Add Japanese (ja) language pack

---
+++ lang/ja/block_oerexchangebrowse.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Japanese language strings for block_oerexchangebrowse.
+ *
+ * @package    block_oerexchangebrowse
+ * @copyright  2026 Adam Jenkins <adam@wisecat.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['licenselabel'] = 'ライセンス: {$a}';
+$string['noresources'] = '公開されているリソースはまだありません。';
+$string['oerexchangebrowse:addinstance'] = '新しい「OER Exchange: 閲覧」ブロックを追加する';
+$string['oerexchangebrowse:myaddinstance'] = '新しい「OER Exchange: 閲覧」ブロックをダッシュボードに追加する';
+$string['pluginname'] = 'OER Exchange: 閲覧';
+$string['privacy:metadata'] = '「OER Exchange: 閲覧」ブロックは local_oerexchange にすでに保存されているデータを表示するのみで、独自のデータは保存しません。';
+$string['searchbutton'] = '検索';
+$string['searchplaceholder'] = 'OER Exchange カタログを検索する';
+$string['viewcatalogue'] = 'カタログ全体を表示する';
